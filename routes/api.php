21082a706dd24da051fe03da641e66facf42a093
William Sant Ana <williamprogphp@yahoo.com.br>
2024-12-09 21:07:51 -0300

    Adicionado rota de api

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\TaskController;
 
 Route::get('/check-api', fn() => response()->json(['status' => 'API working']));
 Route::resource('tasks', TaskController::class);
+Route::get('/csrf-token', fn() => response()->json(['csrf_token' => csrf_token()]));
